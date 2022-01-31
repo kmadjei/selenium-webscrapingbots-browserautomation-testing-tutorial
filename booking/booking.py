9a3a15753e66a75e3d0f8045d45dd639029855d2
Kenneth Adjei <[kmadjei25@gmail.com]>
2022-01-31 18:34:07 -0500

    deleted venv/

---
+++ booking/booking.py
@@ -0,0 +1,3 @@
+import booking.constants as const
+import os
+from selenium import webdriver
---
+++ booking/constants.py
@@ -0,0 +1,2 @@
+PATH = "C:\Program Files (x86)\SeleniumDrivers\chromedriver_win32\chromedriver.exe"
+#BASE_URL = 
